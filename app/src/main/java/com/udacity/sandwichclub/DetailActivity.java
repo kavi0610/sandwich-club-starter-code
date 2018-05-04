6785ad393a114c66937885b398c9171449cc547f
kavi0610 <kavi0610@Kavithas-MacBook-Pro.local>
2018-05-03 19:04:59 -0700

    Pushing changes to Sandwich club app for code review

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,17 +3,24 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +50,11 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
+                //added a place holder to show the default app icon if no image is returned
+                .placeholder(R.mipmap.ic_launcher)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
@@ -56,7 +65,68 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    /*
+     * A helper method to group and set the values to the TextViews
+     * using the sandwich object created
+     */
+
+    private void populateUI(Sandwich sandwich) {
+
+        // Get reference from xml for the TextViews that need to be set
+        TextView alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        TextView IngredientsTv = findViewById(R.id.ingredients_tv);
+        TextView placeOfOriginTv = findViewById(R.id.origin_tv);
+
+        // Create empty Strings for use in formatting values stored in Lists
+        String alsoKnownAsText = "";
+        String ingredientsText = "";
+
+        //Build the text for the also_Known_As TextView to display
+        if (sandwich.getAlsoKnownAs().size() > 0) {
+            alsoKnownAsText = sandwich.getAlsoKnownAs().toString();
+            //remove the opening and closing braces from the string
+            alsoKnownAsText = alsoKnownAsText.substring(1, alsoKnownAsText.length() - 1);
+            alsoKnownAsTv.setText(alsoKnownAsText);
+
+        }
+        // Handle empty values
+        else {
+            alsoKnownAsText = "N/A";
+            alsoKnownAsTv.setText(alsoKnownAsText);
+
+        }
+
+        //Build the text for ingredient_tv TextView to display
+        if (sandwich.getIngredients().size() > 0) {
+            ingredientsText = sandwich.getIngredients().toString();
+            //remove the opening and closing braces from the string
+            ingredientsText = ingredientsText.substring(1, ingredientsText.length() - 1);
+            IngredientsTv.setText(ingredientsText);
+        }
+        // Handle empty values
+        else {
+            ingredientsText = "N/A";
+            IngredientsTv.setText(ingredientsText);
+
+        }
+
+        // Handle empty values in placeOfOrigin
+        if (sandwich.getPlaceOfOrigin().toString().isEmpty()) {
+            placeOfOriginTv.setText("N/A");
+
+        } else {
+            placeOfOriginTv.setText(sandwich.getPlaceOfOrigin().toString());
+        }
+
+        // Handle empty values in description_tv TextView
+        if(sandwich.getDescription().isEmpty()){
+            placeOfOriginTv.setText("N/A");
+        }
+        else {
+            descriptionTv.setText(sandwich.getDescription().toString());
+        }
 
     }
 }
+
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,52 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        ArrayList<String> alsoKnownAsList = new ArrayList<String>();
+        ArrayList<String> ingredientsList = new ArrayList<String>();
+
+        try {
+
+            JSONObject jsonParser = new JSONObject(json);
+
+            //Create another JSONObject to read the name which is a level lower than parent jsonParser
+            JSONObject name = jsonParser.getJSONObject("name");
+            //Parse the name JSONObject to get the mainName
+            String mainName = name.getString("mainName");
+
+            // Use jsonParser to get other elements at same level
+            JSONArray alsoKnownAsJson = name.getJSONArray("alsoKnownAs");
+            if (alsoKnownAsJson.length() > 0) {
+                for (int i = 0; i < alsoKnownAsJson.length(); i++) {
+                    alsoKnownAsList.add(alsoKnownAsJson.getString(i));
+                }
+            }
+            String placeOfOrigin = jsonParser.getString("placeOfOrigin");
+            String description = jsonParser.getString("description");
+            String image = jsonParser.getString("image");
+
+            JSONArray ingredientJson = jsonParser.getJSONArray("ingredients");
+            if (ingredientJson.length()>0){
+                for(int i=0;i< ingredientJson.length();i++ ){
+                    ingredientsList.add(ingredientJson.getString(i));
+                }
+            }
+            // create a new instance of Sandwich object and set the values parsed respectively
+            Sandwich sandwich = new Sandwich(mainName, alsoKnownAsList, placeOfOrigin, description, image, ingredientsList);
+            return sandwich;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 }
